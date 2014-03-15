6f298e9a8a5890bd5cf6602ab82fe1d40ea0fd6e
tobi3988 <tobias.s.bachmann@gmail.com>
2014-03-15 11:30:21 +0100

    created package ...yarn.applicationmaster

--- src/main/scala/com/signalcollect/yarn/applicationmaster/ApplicationMaster.scala
+++ src/main/scala/com/signalcollect/yarn/applicationmaster/ApplicationMaster.scala
@@ -1,28 +1,24 @@
-package com.signalcollect.nodeprovisioning.yarn
+package com.signalcollect.yarn.applicationmaster
 
-import java.nio.ByteBuffer;
+import java.nio.ByteBuffer
 import org.apache.hadoop.net.NetUtils
-import org.apache.hadoop.yarn.api.protocolrecords.FinishApplicationMasterRequest
-import org.apache.hadoop.yarn.api.protocolrecords.RegisterApplicationMasterResponse
 import org.apache.hadoop.yarn.api.records._
 import org.apache.hadoop.yarn.conf.YarnConfiguration
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.yarn.exceptions.YarnException
-import org.apache.hadoop.yarn.client.api.AMRMClient
-import org.apache.hadoop.yarn.client.api.AMRMClient.ContainerRequest;
-import org.apache.hadoop.yarn.client.api.async.AMRMClientAsync.CallbackHandler
+import org.apache.hadoop.yarn.client.api.AMRMClient.ContainerRequest
 import org.apache.hadoop.yarn.client.api.async.AMRMClientAsync
 import org.apache.hadoop.yarn.client.api.async.NMClientAsync
 import org.apache.hadoop.yarn.client.api.async.impl.NMClientAsyncImpl
 import org.apache.hadoop.yarn.util.Records
 import com.signalcollect.util.LogHelper
-import scala.App
 import scala.collection.JavaConversions._
-import scala.collection.mutable.HashMap
+import org.apache.hadoop.yarn.api.records.Priority
+import org.apache.hadoop.yarn.api.records.Resource
 
-class ApplicationMaster(config: Configuration = new YarnConfiguration()) extends App with LogHelper {
+object ApplicationMaster extends App with LogHelper {
   run()
   def run() {
+    val config: Configuration = new YarnConfiguration()
     val allocListener = new RMCallbackHandler()
     val amRMClient :AMRMClientAsync[ContainerRequest] = AMRMClientAsync.createAMRMClientAsync(1000, allocListener)
     amRMClient.init(config)
@@ -64,35 +60,6 @@ class ApplicationMaster(config: Configuration = new YarnConfiguration()) extends
     val request = new ContainerRequest(capability, null, null, pri)
     log.info("Requested container ask: " + request.toString())
     request
-  }
-}
-
-class RMCallbackHandler extends AMRMClientAsync.CallbackHandler with LogHelper {
-	
-  var containers: HashMap[ContainerId, Container] = new HashMap[ContainerId, Container]()
-  override def onContainersCompleted(completedContainers: java.util.List[ContainerStatus]): Unit = {
-    log.info("Got response from RM for container ask, completedCnt="
-      + completedContainers.size)
-
-  }
-
-  override def onContainersAllocated(allocatedContainers: java.util.List[Container]) {
-    log.info("Got response from RM for container ask, allocatedCnt="
-      + allocatedContainers.size)
-  }
-
-  override def onShutdownRequest() {
-    log.info("shutdownRequest")
-  }
-
-  override def onNodesUpdated(updatedNodes: java.util.List[NodeReport]) {}
-
-  override def getProgress(): Float = {
-    0.1f
-  }
-
-  override def onError(e: Throwable) {
-    log.info("onError")
   }
 }
 
---
+++ src/test/scala/com/signalcollect/yarn/applicationmaster/ApplicationMasterSpec.scala
@@ -0,0 +1,44 @@
+package com.signalcollect.yarn.applicationmaster
+
+import com.signalcollect.util.LogHelper
+import com.typesafe.config.ConfigFactory
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.apache.hadoop.yarn.api.records.YarnApplicationState
+import org.specs2.mutable.SpecificationWithJUnit
+import com.signalcollect.nodeprovisioning.yarn.YarnClientFactory
+import com.signalcollect.nodeprovisioning.yarn.YarnDeploymentClient
+import scala.collection.JavaConversions._
+import org.apache.hadoop.yarn.api.records.FinalApplicationStatus
+
+@RunWith(classOf[JUnitRunner])
+class ApplicationMasterSpec extends SpecificationWithJUnit with LogHelper {
+  "ApplicationMaster" should {
+    val typesafeConfig = ConfigFactory.load("test-deployment")
+    val yarnClientFactory = new YarnClientFactory(config = typesafeConfig)
+    val yarnClient = yarnClientFactory.yarnClient
+    val client = new YarnDeploymentClient(typesafeConfig, yarnClientFactory)
+
+    "run application successfull" in {
+      client.submitApplication()
+      var finished = false
+      val apps = yarnClient.getApplications
+      while (!finished) {
+        if (apps.size() == 0) {
+          Thread.sleep(10)
+          log.debug("No Applications found")
+        } else {
+          log.debug(apps.size() + " application(s) found")
+          val appReport = apps.get(apps.size -1)
+          
+          if (appReport.getYarnApplicationState() == YarnApplicationState.FINISHED) {
+            finished = true
+          }
+        }
+      }
+      val appReport = apps.get(apps.size -1)
+      println(appReport.getApplicationId())
+      appReport.getFinalApplicationStatus() === FinalApplicationStatus.SUCCEEDED
+    }
+  }
+}
