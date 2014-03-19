197505be4b942dd82a90a432bce390625e3e8d3f
tobi3988 <tobias.s.bachmann@gmail.com>
2014-03-19 15:57:26 +0100

    change jarTests

--- src/main/scala/com/signalcollect/yarn/deployment/JarCreator.scala
+++ src/main/scala/com/signalcollect/yarn/deployment/JarCreator.scala
@@ -1,11 +1,11 @@
 package com.signalcollect.yarn.deployment
 
-import org.apache.hadoop.util.JarFinder
+import com.signalcollect.yarn.utility.JarUtility
 import java.nio.file.Paths
 import com.signalcollect.yarn.applicationmaster.ApplicationMaster
 
 class JarCreator[T](implicit tag: reflect.ClassTag[T]) {
-	lazy val jarToAppMaster = JarFinder.getJar(ApplicationMaster.getClass())
+	lazy val jarToAppMaster = JarUtility.getJar(ApplicationMaster.getClass())
 	lazy val fullPath = Paths.get(jarToAppMaster)
 	lazy val path = fullPath.getParent.toString + "/"
 	lazy val fileName = fullPath.getFileName.toString
