c73b1de2f70f7ac2af79271e4e55c8263e381559
tobi3988 <tobias.s.bachmann@gmail.com>
2014-03-30 12:42:00 +0200

    changed unmanaged to 2.3.0

---
+++ src/test/scala/com/signalcollect/yarn/deployment/JarTestsSpec.scala
@@ -0,0 +1,28 @@
+package com.signalcollect.yarn.deployment
+
+import org.junit.runner.RunWith
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class JarTestsSpec extends SpecificationWithJUnit {
+ "JarTests" should {
+   "run created jar locally" in {
+     val klass = HelloWorld.getClass()
+     val pathToJar = JarCreator.createJarFile(klass)
+     val fullClassName = klass.getName()
+     val className = fullClassName.substring(0, fullClassName.length()-1)
+     LocalJarRunner.run(pathToJar, className) must not(throwAn[Exception])
+   }
+   
+   "create jar file out of several classes and run them" in {
+     val klass1 = FirstDummy.getClass()
+     val klass2 = SecondDummy.getClass()
+     val pathToJar = JarCreator.createJarFile(klass1)
+     val fullClassName = klass1.getName()
+     val className = fullClassName.substring(0, fullClassName.length()-1)
+     LocalJarRunner.run(pathToJar, className) must not(throwAn[Exception])
+   }
+   
+ }
+}
