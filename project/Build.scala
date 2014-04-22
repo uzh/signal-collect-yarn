787b73deee85285dced178f5a1b7e6d68025c2af
tobi3988 <tobias.s.bachmann@gmail.com>
2014-04-22 17:45:13 +0200

    changed ProjectRef to github

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,11 @@ import sbt._
 import Keys._
 
 object GraphsBuild extends Build {
+
 	lazy val scCore = ProjectRef(uri("git://github.com/uzh/signal-collect.git"), "signal-collect")
+
+//	lazy val scCore = ProjectRef(file("../signal-collect"), "signal-collect")
+
   val scYarn = Project(id = "signal-collect-yarn",
     base = file(".")) dependsOn (scCore)
 }
