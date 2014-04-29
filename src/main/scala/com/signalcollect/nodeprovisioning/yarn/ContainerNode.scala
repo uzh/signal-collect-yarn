7ea7eec010276d14db98d4f8dd58018cdad52245
tobi3988 <tobias.s.bachmann@gmail.com>
2014-04-29 13:48:33 +0200

    start actor system in node

--- src/main/scala/com/signalcollect/nodeprovisioning/yarn/ContainerNode.scala
+++ src/main/scala/com/signalcollect/nodeprovisioning/yarn/ContainerNode.scala
@@ -1,5 +1,35 @@
 package com.signalcollect.nodeprovisioning.yarn
 
+import akka.actor.ActorSystem
+import com.signalcollect.configuration.ActorSystemRegistry
+import com.signalcollect.configuration.AkkaConfig
+import akka.event.Logging
+import com.signalcollect.nodeprovisioning.AkkaHelper
 
-class ContainerNode(id: Int, baseport: Int = 2552) {
+
+class ContainerNode(id: Int, baseport: Int = 2552,  kryoRegistrations: List[String] = Nil, kryoInit: String = "com.signalcollect.configuration.KryoInit") {
+  val system = ActorSystemRegistry.retrieve("SignalCollect").getOrElse(startActorSystem)
+  
+  val akkaPort = baseport + id + 1
+    
+  def startActorSystem: ActorSystem = {
+    println("start Actorsystem")
+    try {
+      val system = ActorSystem("SignalCollect", akkaConfig(akkaPort, kryoRegistrations))
+      ActorSystemRegistry.register(system)
+      system
+    } catch {
+      case e: Exception => {
+        throw e
+      }
+    }
+  }
+  
+  def akkaConfig(akkaPort: Int, kryoRegistrations: List[String]) = AkkaConfig.get(
+    akkaMessageCompression = true,
+    serializeMessages = true,
+    loggingLevel = Logging.WarningLevel, //Logging.DebugLevel,Logging.WarningLevel
+    kryoRegistrations = kryoRegistrations,
+    kryoInitializer = kryoInit,
+    port = akkaPort)
 }
--- src/test/scala/com/signalcollect/nodeprovisioning/yarn/ContainerNodeSpec.scala
+++ src/test/scala/com/signalcollect/nodeprovisioning/yarn/ContainerNodeSpec.scala
@@ -6,19 +6,28 @@ import org.specs2.mutable.SpecificationWithJUnit
 import com.signalcollect.configuration.ActorSystemRegistry
 
 @RunWith(classOf[JUnitRunner])
-class ContainerNodeSpec extends SpecificationWithJUnit {
- "ContainerNode" should {
-   "be created" in {
-    val container = new ContainerNode(0)
-    container must not be None
-   }
+class ContainerNodeSpec extends SpecificationWithJUnit with StopActorSystemAfter {
+ "ContainerNode creation" should {
+//   "be created" in  {
+//    val container = new ContainerNode(0)
+//    container must not be None
+//   }
    
-   "container node should start actor system" in {
+   "container node should start actor system" in  {
      val id = 0
      val baseport = 2552
      val container = new ContainerNode(id = id, baseport = baseport)
      container must not be None
      ActorSystemRegistry.retrieve("SignalCollect").isDefined === true
+    
    }
+   
+   "receive shutdown message" in  {
+     val container = new ContainerNode(0)
+     container must not be None
+   }
+   
  }
+ 
+
 }
