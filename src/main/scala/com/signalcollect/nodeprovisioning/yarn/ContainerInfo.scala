df3d3df8fa6b57e1d48ede14beea437840265208
tobi3988 <tobias.s.bachmann@gmail.com>
2014-04-29 21:18:37 +0200

    changed to case class

--- src/main/scala/com/signalcollect/nodeprovisioning/yarn/ContainerInfo.scala
+++ src/main/scala/com/signalcollect/nodeprovisioning/yarn/ContainerInfo.scala
@@ -3,7 +3,7 @@ package com.signalcollect.nodeprovisioning.yarn
 /**
  * Every Container has an unique id and an actor system that runs on port 'baseport + id + 1'
  */
-class ContainerInfo(val ip: String, val id: Int, val basePort: Int = 2552) {
+case class ContainerInfo(val ip: String, val id: Int, val basePort: Int = 2552) {
   def actorAddress: String = {
     val systemId = id + 1
     val containerPort = basePort + id + 1
