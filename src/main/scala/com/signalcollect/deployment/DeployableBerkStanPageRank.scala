8ebafeaae9d5b062229120b4d4e037fa3d1c7ec7
tobi3988 <tobias.s.bachmann@gmail.com>
2014-05-31 10:45:51 +0200

    created BerkStanPageRank

--- src/main/scala/com/signalcollect/deployment/DeployableBerkStanPageRank.scala
+++ src/main/scala/com/signalcollect/deployment/DeployableBerkStanPageRank.scala
@@ -13,35 +13,39 @@ import akka.actor.ActorRef
 
 class DeployableBerkStanPageRank extends YarnDeployableAlgorithm {
   def execute(parameters: Map[String, String], nodeActors: Array[ActorRef]) {
-    val graph = GraphBuilder.withPreallocatedNodes(nodeActors).withShutdownActorSystem(false).build
-    val filename = "web-BerkStan.txt"
-    val in = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))
-    var line = in.readLine
-    while (line != null) {
-      if (!line.startsWith("#")) {
-        val edgeTuple = line.split("	")
-        val nextSource = toInt(edgeTuple(0))
-        val nextTarget = toInt(edgeTuple(1))
-        addVerticesAndEdge(nextSource, nextTarget, graph)
+    try {
+      val graph = GraphBuilder.withPreallocatedNodes(nodeActors).withShutdownActorSystem(false).build
+      val filename = parameters.get("filename").getOrElse("web-BerkStan.txt")
+      val in = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))
+      var line = in.readLine
+      println("read graph from file")
+      while (line != null) {
+        if (!line.startsWith("#")) {
+          val edgeTuple = line.split("	")
+          val nextSource = toInt(edgeTuple(0))
+          val nextTarget = toInt(edgeTuple(1))
+          addVerticesAndEdge(nextSource, nextTarget, graph)
+        }
+        line = in.readLine
       }
-      line = in.readLine
+
+      println("Graph has been built, awaiting idle ...")
+      graph.awaitIdle
+      println("Executing computation ...")
+      val stats = graph.execute
+      println(stats)
+      graph.shutdown
+    } catch {
+      case e: Exception => e.printStackTrace()
     }
-
-    println("Graph has been built, awaiting idle ...")
-    graph.awaitIdle
-    println("Executing computation ...")
-    val stats = graph.execute
-    println(stats)
-    graph.shutdown
-
   }
-  def addVerticesAndEdge(source: Int, target: Int,graph: Graph[Any,Any]) {
-	  graph.addVertex(new PageRankVertex(source))
-	  graph.addVertex(new PageRankVertex(target))
-	  graph.addEdge(source, new PageRankEdge(target))
+  def addVerticesAndEdge(source: Int, target: Int, graph: Graph[Any, Any]) {
+    graph.addVertex(new PageRankVertex(source))
+    graph.addVertex(new PageRankVertex(target))
+    graph.addEdge(source, new PageRankEdge(target))
   }
-  
+
   def toInt(s: String): Int = {
-		  Integer.valueOf(s)
+    Integer.valueOf(s)
   }
 }
