1c409f76d947d6c59815b72d29718d0524314e48
tobi3988 <tobias.s.bachmann@gmail.com>
2014-06-03 18:01:02 +0200

    clone signal-collect before assembly

--- project/Build.scala
+++ project/Build.scala
@@ -3,9 +3,9 @@ import Keys._
 
 object GraphsBuild extends Build {
 
-	lazy val scCore = ProjectRef(uri("git://github.com/uzh/signal-collect.git#scala-2.11-akka-2.3.2-migration"), "signal-collect")
+//	lazy val scCore = ProjectRef(uri("git://github.com/uzh/signal-collect.git#scala-2.11-akka-2.3.2-migration"), "signal-collect")
 
-//	lazy val scCore = ProjectRef(file("../signal-collect"), "signal-collect")
+	lazy val scCore = ProjectRef(file("../signal-collect"), "signal-collect")
 
   val scYarn = Project(id = "signal-collect-yarn",
     base = file(".")) dependsOn (scCore)
