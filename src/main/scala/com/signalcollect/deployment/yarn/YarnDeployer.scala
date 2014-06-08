f6e4141d40cc75cb81521554cfa54ada4b614dbb
tobi3988 <tobias.s.bachamnn@Muhh>
2014-06-08 16:17:10 +0200

    added license and organized imports

--- src/main/scala/com/signalcollect/deployment/yarn/YarnDeployer.scala
+++ src/main/scala/com/signalcollect/deployment/yarn/YarnDeployer.scala
@@ -1,20 +1,29 @@
+/*
+ *  @author Tobias Bachmann
+ *
+ *  Copyright 2014 University of Zurich
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 package com.signalcollect.deployment.yarn
 
-import com.signalcollect.yarn.applicationmaster.ApplicationMaster
-import com.typesafe.config.ConfigFactory
-import scala.collection.JavaConversions._
-import org.apache.hadoop.yarn.api.records.YarnApplicationState
-import com.signalcollect.util.ConfigProvider
-import com.signalcollect.deployment.DeploymentConfigurationCreator
-import com.signalcollect.deployment.Cluster
-import com.signalcollect.deployment.DeploymentConfiguration
-import org.apache.hadoop.yarn.api.records.ApplicationId
 import com.signalcollect.deployment.ClusterCreator
+import com.signalcollect.deployment.DeploymentConfigurationCreator
 
 object YarnDeployer extends App {
   val deploymentConf = DeploymentConfigurationCreator.getDeploymentConfiguration
   val cluster = ClusterCreator.getCluster(deploymentConf)
   cluster.deploy(deploymentConf)
-
 }
 
