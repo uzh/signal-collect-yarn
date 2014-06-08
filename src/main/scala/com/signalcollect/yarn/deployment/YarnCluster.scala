b9f8e398c58fdeb6114ff4b78c54f7514b36db1e
tobi3988 <tobias.s.bachamnn@Muhh>
2014-06-08 15:10:53 +0200

    created YarnCluster

---
+++ src/main/scala/com/signalcollect/yarn/deployment/YarnCluster.scala
@@ -0,0 +1,46 @@
+package com.signalcollect.yarn.deployment
+
+import com.signalcollect.yarn.applicationmaster.ApplicationMaster
+import com.typesafe.config.ConfigFactory
+import scala.collection.JavaConversions._
+import org.apache.hadoop.yarn.api.records.YarnApplicationState
+import com.signalcollect.util.ConfigProvider
+import com.signalcollect.deployment.DeploymentConfigurationCreator
+import com.signalcollect.deployment.Cluster
+import com.signalcollect.deployment.DeploymentConfiguration
+import org.apache.hadoop.yarn.api.records.ApplicationId
+
+class YarnCluster extends Cluster {
+  lazy val yarnClient = YarnClientCreator.yarnClient
+  override def deploy(deploymentConfiguration: DeploymentConfiguration) {
+
+    val launchSettings = LaunchSettingsCreator.getSettingsForClass(ApplicationMaster.getClass(), deploymentConfiguration)
+    val client = new YarnDeploymentClient(launchSettings)
+    val application = client.submitApplication()
+    waitForTermination(application)
+  }
+
+  def waitForTermination(application: ApplicationId) {
+    var finished = false
+    while (!finished) {
+      Thread.sleep(1000)
+      val apps = yarnClient.getApplications.toList
+      if (apps.size() == 0) {
+        Thread.sleep(10)
+      } else {
+        if (apps.exists(_.getApplicationId().equals(application))) {
+          val appReport = apps.find(_.getApplicationId().equals(application)).get
+          val applicationState = appReport.getYarnApplicationState()
+          println("ApplicationState = " + applicationState)
+          if (applicationState == YarnApplicationState.FINISHED ||
+            applicationState == YarnApplicationState.FAILED ||
+            applicationState == YarnApplicationState.KILLED) {
+            finished = true
+          }
+        }
+      }
+    }
+  }
+
+}
+
--- src/main/scala/com/signalcollect/yarn/deployment/YarnDeployer.scala
+++ src/main/scala/com/signalcollect/yarn/deployment/YarnDeployer.scala
@@ -6,33 +6,15 @@ import scala.collection.JavaConversions._
 import org.apache.hadoop.yarn.api.records.YarnApplicationState
 import com.signalcollect.util.ConfigProvider
 import com.signalcollect.deployment.DeploymentConfigurationCreator
+import com.signalcollect.deployment.Cluster
+import com.signalcollect.deployment.DeploymentConfiguration
+import org.apache.hadoop.yarn.api.records.ApplicationId
+import com.signalcollect.deployment.ClusterCreator
 
 object YarnDeployer extends App {
-  val yarnClient = YarnClientCreator.yarnClient
-  val launchSettings = LaunchSettingsCreator.getSettingsForClass(ApplicationMaster.getClass(), DeploymentConfigurationCreator.getDeploymentConfiguration)
-  val client = new YarnDeploymentClient(launchSettings)
-  val application = client.submitApplication()
-  waitForTermination
-  
-  def waitForTermination {
-    var finished = false
-    while (!finished) {
-      Thread.sleep(1000)
-      val apps = yarnClient.getApplications.toList
-      if (apps.size() == 0) {
-        Thread.sleep(10)
-      } else {
-        if (apps.exists(_.getApplicationId().equals(application))) {
-          val appReport = apps.find(_.getApplicationId().equals(application)).get
-          val applicationState = appReport.getYarnApplicationState()
-          println("ApplicationState = " + applicationState)
-          if (applicationState == YarnApplicationState.FINISHED || applicationState == YarnApplicationState.FAILED) {
-            finished = true
-          }
-        }
-      }
-    }
-  }
+  val deploymentConf = DeploymentConfigurationCreator.getDeploymentConfiguration
+  val cluster = ClusterCreator.getCluster(deploymentConf)
+  cluster.deploy(deploymentConf)
 
 }
 
